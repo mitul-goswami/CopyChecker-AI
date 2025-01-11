c6e977ea4a255015c604959f50e799957f197715
V-Silpin <varun.mohanta323@gmail.com>
2025-01-11 17:02:12 +0530

    Tested ocr route; worked well, next step is to test Cloud Vision API

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,3 +1,25 @@
-import PyPDF2
+from fastapi import FastAPI, Request, UploadFile, File, HTTPException
+from http import HTTPStatus
+import PyPDF2 as pdf
+import io
 
-print(PyPDF2.__version__)
+app = FastAPI()
+
+@app.post('/ocr')
+async def ocr(file: UploadFile = File(...)):
+    filename = file.filename
+    
+    if ".pdf" not in filename:
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail="file type is not pdf",
+        )
+    
+    pdf_file = await file.read()
+    pdf_stream = io.BytesIO(pdf_file)
+    reader = pdf.PdfReader(pdf_stream)
+    no = len(reader.pages)
+    return {
+        "filename": filename,
+        "page_count": no
+    }
